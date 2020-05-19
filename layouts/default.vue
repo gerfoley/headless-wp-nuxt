7f2e2c7b9abdbe9b224de3f3696254854ef67102
Gerry Foley <gfoley@internationalliving.com>
2020-05-19 14:42:56 +0100

    adding styles testing

--- layouts/default.vue
+++ layouts/default.vue
@@ -90,4 +90,98 @@ code {
 p {
   margin: 15px 0 20px;
 }
+.ilsignup {
+    background-color: #e5eaf0;
+    border: .0625rem solid #d4e1e7;
+    display: block;
+    float: left;
+    margin: 1.5rem 0;
+    overflow: hidden;
+    padding: 1.25rem 1.25rem 1rem;
+    position: relative;
+    width: 100%
+}
+
+.ilsignup h2.center {
+    font-size: 1.625rem;
+    letter-spacing: .025rem;
+    line-height: 1;
+    margin-bottom: 1.25rem;
+    text-align: center
+}
+
+.ilsignup p {
+    font-family: arial,sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    letter-spacing: -.025rem;
+    line-height: 1.6875;
+    margin-bottom: 2.4375rem;
+    word-wrap: break-word
+}
+
+body.home .ilsignup p {
+    height: 8.4375rem;
+    overflow: hidden
+}
+
+.ilsignup p a {
+    font-family: arial,sans-serif;
+    font-weight: 700
+}
+
+.ilsignup form {
+    overflow: hidden
+}
+
+.ilsignup button[type=submit],.ilsignup form,.ilsignup input[type=email] {
+    display: block;
+    float: left;
+    width: 100%
+}
+
+.ilsignup input[type=email] {
+    background: #fff url(assets/images/svg-icons/email-light.svg) no-repeat 15px 16px;
+    background-size: 1.6875rem 1.125rem;
+    color: #3b3b45;
+    content: " ";
+    display: block;
+    margin-bottom: 1.25rem;
+    padding-left: 3.5625rem
+}
+
+.ilsignup button[type=submit] {
+    background-color: #fabd29;
+    background-image: linear-gradient(0deg,#fabd29 0,#ffc945);
+    border: .0625rem solid #ffb500;
+    border-radius: .25rem;
+    font-size: 1.125rem;
+    letter-spacing: .15625rem
+}
+
+.ilsignup button[type=submit]:active,.ilsignup button[type=submit]:hover {
+    background-color: #eba805;
+    background-image: none;
+    cursor: pointer
+}
+
+.ilsignup button[type=submit]:focus {
+    background-color: #eba805;
+    background-image: none
+}
+
+.ilsignup .spam-pledge {
+    display: block;
+    float: left;
+    overflow: hidden;
+    text-align: center;
+    width: 100%
+}
+
+.ilsignup .spam-pledge a {
+    color: #152d6e;
+    font-size: .875rem;
+    line-height: 1.4;
+    text-decoration: none
+}
 </style>
